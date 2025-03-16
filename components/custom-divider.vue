e48b61ede0f4fb5ba4d0cf007524c465730465ec
Jefferson <jeffersondevlsilva@gmail.com>
2025-03-16 20:36:47 -0300

    feat: create new component 'custom-divider'

---
+++ components/custom-divider.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex custom-divider">
+    <span class="vertical-divider"></span>
+  </div>
+</template>
+<style scoped>
+.custom-divider {
+  background-color: white;
+}
+
+.custom-divider span {
+  width: 1px;
+  height: 60%;
+  background-color: var(--grey);
+  display: inline-block;
+  position: relative;
+  align-self: center;
+}
+</style>
