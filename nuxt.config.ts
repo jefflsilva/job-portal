2b8184e51057425319a01401164453a8bcf2cd5a
Jefferson <jeffersondevlsilva@gmail.com>
2025-03-16 14:57:11 -0300

    feat: criando e configurando css global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  css: ["~/public/css/global.css"],
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+  ],
+});
