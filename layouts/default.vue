b98d97a7af3d4b342c75bb022474c828c6404752
Jefferson <jeffersondevlsilva@gmail.com>
2025-03-16 14:58:36 -0300

    feat: criando layout e tela inicial

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <CustomNavbar />
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+}
+
+.container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url("../public/images/main-bg-image.jpg") center/cover no-repeat;
+  filter: blur(30px);
+  z-index: -1;
+}
+.container h1 {
+  color: white;
+}
+
+.container::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: -1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="hero-section">
+    <div class="main-section">
+      <div class="text-white">
+        <h1>Find Your Dream Job Today!</h1>
+        <p class="text-center">
+          Connecting Talent with Opportunity: Your Gateway to Career Success
+        </p>
+      </div>
+    </div>
+    <div class="footer-section">logos</div>
+  </section>
+</template>
+<style scoped>
+.hero-section {
+  min-height: calc(100vh - 64px);
+  display: flex;
+  flex-direction: column;
+}
+
+.hero-section h1 {
+  font-size: 64px;
+}
+
+.main-section {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.footer-section {
+  background: black;
+  height: 128px;
+}
+</style>
